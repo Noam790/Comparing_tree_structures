bec60865b391298fe024b1f14ec03db8c8e00666
Noam <noamchamouard@outlook.fr>
2025-10-09 10:59:51 +0200

    project base

---
+++ debug/tree/CMakeFiles/test-avl-tree.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for test-avl-tree.
+# This may be replaced when dependencies are built.
---
+++ debug/tree/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Utilisateurs/nchamoua/Documents/L3/Comparing_tree_structures/src/tree
+# Build directory: /Utilisateurs/nchamoua/Documents/L3/Comparing_tree_structures/debug/tree
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test-avl-tree "./test-avl-tree")
+set_tests_properties(test-avl-tree PROPERTIES  _BACKTRACE_TRIPLES "/Utilisateurs/nchamoua/Documents/L3/Comparing_tree_structures/src/tree/CMakeLists.txt;57;add_test;/Utilisateurs/nchamoua/Documents/L3/Comparing_tree_structures/src/tree/CMakeLists.txt;0;")
